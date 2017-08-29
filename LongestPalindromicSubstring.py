01feecc5510f26d4eff8c1910f8960a8ccd5e1ab
yangrui123 <m18330219154@163.com>
2017-08-29 16:08:36 +0800

    modified

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -15,13 +15,13 @@ def longestPalindrome(s):
     for i in range(n):
         p[i][i]=1
     max_len = 1
-    rstr = s[0]
+    start, end = 0,1
     i, j = 0,1
     while j<n:
         if s[i]==s[j]:
             p[i][j]=1
             max_len=2
-            rstr = s[i:j+1]
+            start, end = i, j+1
         i+=1;j+=1
     for sep in range(2,n):
         for row in range(n-sep):
@@ -31,41 +31,31 @@ def longestPalindrome(s):
                 tlen = col - row + 1
                 if tlen > max_len:
                     max_len = tlen
-                    rstr = s[row:col + 1]
-    return rstr
+                    start, end = row, col+1
+    return s[start:end]
 
 def longestPalindrome2(s):
     n = len(s)
-    if n==0 or n==1:
+    if n == 0 or n == 1:
         return s
-    p = [[0 for col in range(n)] for row in range(n)]
-    for sep in range(n):
-        for row in range(n):
-            col = row+sep
-            if col >= n:
-                break
-            if s[row]==s[col]:
-                if col-1>=row+1:
-                    if p[row+1][col-1]==1:
-                        p[row][col] = 1
-                else:
-                    p[row][col] = 1
-    max_len = 0
-    rstr = ""
-    for i in range(n):
-        for j in range(i,n):
-            if p[i][j] == 1:
-                tlen = j-i+1
-                if tlen > max_len:
-                    max_len = tlen
-                    rstr = s[i:j+1]
-    return rstr
+    maxLen = 1
+    start = 0
+    for i in xrange(n):
+        if i - maxLen >= 1 and s[i - maxLen - 1:i + 1] == s[i - maxLen - 1:i + 1][::-1]:
+            start = i - maxLen - 1
+            maxLen += 2
+            continue
+
+        if i - maxLen >= 0 and s[i - maxLen:i + 1] == s[i - maxLen:i + 1][::-1]:
+            start = i - maxLen
+            maxLen += 1
+    return s[start:start + maxLen]
+
 
 
 if __name__ == '__main__':
-    s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    #s = 'aaaa'
+    s="ranynarcivilwartestingwhetherthatnaptionotionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
     begin = time.time()
-    res = longestPalindrome(s)
+    res = longestPalindrome2(s)
     print time.time()-begin
     print res
